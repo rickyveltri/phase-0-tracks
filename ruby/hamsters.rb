f93e24081d08a158fb67dbbac889e43abc516c83
rickyveltri <rickveltri@gmail.com>
2017-04-21 12:22:35 -0700

    Release 1

---
+++ ruby/hamsters.rb
@@ -0,0 +1,15 @@
+puts "What is the hamster's name?"
+hamsterName = gets.chomp
+
+puts "On a scale from 1-10, how squeeky is the hamster?"
+volLevel = gets.chomp
+
+puts "What color is the hamster?"
+furColor = gets.chomp
+
+puts "Good candidate for adoption? (Y/N)"
+candidate = gets.chomp
+
+puts "Estimated age?"
+age = nil
+age = gets.chomp
