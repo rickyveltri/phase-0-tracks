f70f81753f2cb7d79fa33f13af33bf9f75eb665d
rickyveltri <rickveltri@gmail.com>
2017-04-23 16:58:32 -0700

    Add data evaluation

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,49 +1,41 @@
 #DATA COLLECTION
 
-puts "What is your name?"
+puts 'What is your name?'
 name = gets.chomp
 
-
-puts "How old are you?"
+puts 'How old are you?'
 age = gets.chomp.to_i
 
-
-puts "What year were you born?"
+puts 'In what year were you born?'
 birth_year = gets.chomp.to_i
 
-
-calculated_age = Time.new.year - birth_year
-age_correct = calculated_age == age
-puts age_correct
-
-
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+puts 'Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)'
 garlic_bread = gets.chomp
 
-
-puts "Would you like to enroll in the company’s health insurance?"
+puts 'Would you like to enroll in the company’s health insurance? (yes/no)'
 health_insurance = gets.chomp
 
 
 
+# DATA EVALUATION
 
+#age evaluation
+calculated_age = Time.new.year - birth_year
+age_correct = calculated_age == age
 
-=begin
-DATA EVALUATION
-If the employee got their age right, and is willing to eat garlic bread or sign up for insurance, the result is “Probably not a vampire.”
-If the employee got their age wrong, and hates garlic bread or waives insurance, the result is “Probably a vampire.”
-If the employee got their age wrong, hates garlic bread, and doesn’t want insurance, the result is “Almost certainly a vampire.”
-Even if the employee is an amazing liar otherwise, anyone going by the name of “Drake Cula” or “Tu Fang” is clearly a vampire, because come on. In that case, you should print “Definitely a vampire.”
-Otherwise, print “Results inconclusive.”
-=end
-
-#age correct and willing to eat garlic or sign up for insurance
-
-#name evaluation
+# name evaluation, which trumps all conditions
 if name == 'Drake Cula' || name == 'Tu Fang'
-  name_eval = 'Definitely a vampire.'
+  puts 'Definitely a vampire.'
+  #age correct and yes to either garlic bread or health insurance
+  elsif age_correct && (garlic_bread == 'yes' || health_insurance == 'yes')
+  puts 'Probably not a vampire.'
+  #age incorrect and no to both garlic bread and health insurance
+  elsif !age_correct && garlic_bread == 'no' && health_insurance == 'no'
+  puts 'Almost certainly a vampire'
+  #age incorrect and declined either garlic bread or health insurance
+  elsif !age_correct && (garlic_bread == 'no' || health_insurance == 'no')
+  puts 'Probably a vampire.'
+else
+  puts 'Results inconclusive'
 end
-puts name_eval
-
-
 
