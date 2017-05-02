7c5d8598b4e5308a1c8bee02ea8f6db0dbf8cb49
rickyveltri <rickveltri@gmail.com>
2017-05-02 14:23:20 -0700

    Define the Santa class

---
+++ ruby/santa.rb
@@ -0,0 +1,15 @@
+class Santa
+# Define speak method
+  def speak
+    puts "Ho, ho, ho!  Haaaappy holidays!"
+  end
+# Define eat_milk_and_cookies method
+  def eat_milk_and_cookies(cookie_type)
+    puts "That was a good #{cookie_type}!"
+  end
+# Define initialize method
+  def initialize
+    puts "Initializing Santa instance"
+  end
+
+end
