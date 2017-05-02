8ca5398fb43e98c9aa848d2f19d9aebe58786578
rickyveltri <rickveltri@gmail.com>
2017-05-02 16:22:22 -0700

    Create class: Santa

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,15 +1,22 @@
 class Santa
+
+# Define initialize method
+  def initialize
+    puts "Initializing Santa instance..."
+  end
+
 # Define speak method
   def speak
     puts "Ho, ho, ho!  Haaaappy holidays!"
   end
+
 # Define eat_milk_and_cookies method
   def eat_milk_and_cookies(cookie_type)
-    puts "That was a good #{cookie_type}!"
-  end
-# Define initialize method
-  def initialize
-    puts "Initializing Santa instance"
+    puts "That was a good #{cookie_type} cookie!"
   end
 
 end
+
+santa_claus = Santa.new
+santa_claus.speak
+santa_claus.eat_milk_and_cookies('oatmeal')
