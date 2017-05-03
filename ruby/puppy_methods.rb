c5d8ed0b3e345de71794583b01a4ae389f97b0b3
rickyveltri <rickveltri@gmail.com>
2017-05-03 10:21:15 -0700

    Complete release 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,41 @@ class Puppy
     toy
   end
 
+  #define speak method, printing "woof" as many times as the argument indicates
+  def speak(integer)
+    integer.times do
+    puts "Woof!"
+  end
+  end
+
+  #create roll over method
+  def roll_over
+    puts "*roll over*"
+  end
+
+  #create dog years method, which takes human years (1) and turns it into dog years (7)
+  def dog_years(integer)
+    dog_years = integer*7
+    puts "#{integer} human years is equal to #{dog_years} dog years."
+  end
+
+  #have the dog play dead.
+  def play_dead
+    puts "Lay down, roll over, stay still"
+  end
+
 end
+
+rosco = Puppy.new
+
+
+#DRIVER CODE
+rosco.fetch("ball")
+rosco.speak(4)
+rosco.roll_over
+rosco.dog_years(10)
+rosco.play_dead
+
+
+
 
