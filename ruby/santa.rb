e91862201978ab62a244e1f6d81c23f8f9297336
rickyveltri <rickveltri@gmail.com>
2017-05-04 13:36:22 -0700

    Initialize santas and add to array

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -21,6 +21,23 @@ class Santa
 
 end
 
-santa_claus = Santa.new
+santa_claus = Santa.new("agender", "aethnic")
 santa_claus.speak
 santa_claus.eat_milk_and_cookies('oatmeal')
+
+santas = []
+
+santas.push(Santa.new("female", "asian"))
+santas.push(Santa.new("female", "latino"))
+santas.push(Santa.new("female", "black"))
+santas.push(Santa.new("female", "white"))
+santas.push(Santa.new("female", "indian"))
+santas.push(Santa.new("male", "asian"))
+santas.push(Santa.new("male", "latino"))
+santas.push(Santa.new("male", "black"))
+santas.push(Santa.new("male", "white"))
+santas.push(Santa.new("male", "indian"))
+
+p santas
+
+
