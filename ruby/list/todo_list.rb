0727c612b001c36df7a5bdb685e02247e133b06c
rickyveltri <rickveltri@gmail.com>
2017-05-05 13:34:19 -0700

    Fill in methods so that all tests pass

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -1,18 +1,28 @@
-
-
 class TodoList
 
+  #Store the list items given on initialization
+  def initialize(array)
+    @array = array
+  end
+
+  #Return array
   def get_items
+    @array
   end
 
-  def add_item
+  #Add item to array
+  def add_item(item)
+    @array << item
   end
 
-  def delete_item
+  #Delete item from array
+  def delete_item(item)
+    @array.delete(item)
   end
 
-
+  #Return item by index
+  def get_item(item_index)
+    @array[item_index]
+  end
 
 end
-
-
