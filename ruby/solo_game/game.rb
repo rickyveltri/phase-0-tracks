bbad30ac0c7a2fc8a97dd7a4dbe9d372efac4ab3
rickyveltri <rickveltri@gmail.com>
2017-05-07 15:03:38 -0700

    save point [work on another project]

--- ruby/solo_game/game.rb
+++ ruby/solo_game/game.rb
@@ -9,9 +9,92 @@
 # The user should get a congratulatory message if they win, and a taunting message if they lose.
 
 
-=begin
 
-Class WordGuess
+
+
+class WordGuess
+
+  #attribute accessors
+  attr_accessor
+
+
+  #initialization method
+  def initialize
+    @answer = ""
+    @answer_letters = @answer.downcase.chars
+    @answer_length = @word_letters.length
+    @player2_view = []
+    @guessed_letters = []
+    @strikes_remaining = 0
+    @current_guess = ""
+    @is_over = false
+
+    #outcome messages
+    @taunt = "Muhahaha, YOU LOSE!"
+    @congratulate = "WINNER!!! YOU ARE THE WORDGUESS MASTER!"
+  end
+
+
+  #receive the word from player1, set the strike count
+  def player1_word_input
+    puts "Player 1: enter the word now:"
+    @answer = gets.chomp
+    #set the strike count
+    @strikes_remaining = (@answer.length*0.75).round
+    #convert to downcase and store as an array
+    @answer_letters = @answer.downcase.chars
+  end
+
+
+  #player2_view
+  def create_player2_view
+    @answer_length.times do
+    @player2_view << "_"
+  end
+
+
+  def display_player2_view
+    @player2_view.join(" ")
+  end
+
+
+  def store_guessed_letter
+    @guessed_letters << @current_guess
+  end
+
+
+  def guess
+    puts "Player 2: guess a letter:"
+    @current_guess = gets.chomp
+    if @guessed_letters.index(@current_guess) != nil
+      puts "You've already guessed #{@current_guess}, silly!  Guess again."
+      guess
+    elsif @answer_letters.index(@current_guess) != nil
+
+
+
+
+  end
+
+  def game_status
+    if word_letters == player2_view
+      @congratulate
+    elsif strikes_remaining == -1
+      @taunt
+      break
+    else
+
+
+
+
+end
+
+
+
+
+
+
+=begin
   *** attributes required ***
 
 
@@ -20,12 +103,6 @@ Class WordGuess
 
   *** methods ***
 
-  Convert word to lowercase
-  convert word or phase to "_ _ _ _ _" format
-
-  display "_ _ _ _ _"
-
-  calculate guess limit, based on word length
 
   Take guess
       evaluate uniqueness
@@ -37,23 +114,6 @@ Class WordGuess
 
   Incorrect: dock from guess total
       Evaluate if guess limit has been reached
-
-
-  Determine win
-      congratulatory message
-
-
-  Determine loss
-      taunt message
-
-
-
-Variables and data structures
-word
-word_letters
-player2_view_arr
-guessed_letters
-
 
 
 
--- ruby/solo_game/game_spec.rb
+++ ruby/solo_game/game_spec.rb
@@ -2,11 +2,11 @@
 
 require_relative 'game'
 
+
+  ### STORE DATA ###
 describe WordGuess do
   let(:word) { WordGuess.new('Hangman') }
 
-
-  ### STORE DATA ###
   it "stores the word given by player1 on initiation" do
     expect(word).to eq['Hangman']
   end
@@ -15,31 +15,32 @@ describe WordGuess do
     expect(word.downcase).to eq ['hangman']
   end
 
-  it "creates the 'invisible' player2 view" do
-    expect(word.player2_view).to_eq [_,_,_,_,_,_,_]
+  it "creates the 'invisible' player2_view array" do
+    expect(word.player2_view).to_eq ['_', '_', '_', '_', '_', '_', '_']
   end
 
-  it "stores a guessed letter" do
+  it "stores letter_guess to guessed_letters array" do
+    expect(guess_letter('a')).to eq
   end
 
   it "checks if a current guess matches a previous guess" do
   end
 
-  it "subtracts one from guesses_remaining if the letter_guess matches" do
+  it "subtracts one from strikes_remaining if the letter_guess matches" do
   end
 
-  it "does not subtract one from guesses_remaining if the letter_guess is a match" do
+  it "does not subtract one from strikes_remaining if the letter_guess is a match" do
   end
 
-  it "updates the player2_view if letter_guess matches word_letters" do
+  it "updates the player2_view if letter_guess matches any word_letters" do
   end
 
-  it "displays a taunt if guesses_remaining reaches -1" do
+  it "displays a taunt if strikes_remaining reaches -1" do
+    expect()
   end
 
 
-
-
+#guess, store, compare, act
 
 
 
@@ -47,3 +48,6 @@ describe WordGuess do
 
 
 end
+
+
+###
