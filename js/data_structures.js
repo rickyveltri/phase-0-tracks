543b6079cc369c1b5b41ef3e9f287527a5b03fc7
rickyveltri <rickveltri@gmail.com>
2017-05-10 12:16:52 -0700

    Add two arrays, then code which pushes additional information to the arrays

---
+++ js/data_structures.js
@@ -0,0 +1,10 @@
+// Add two arrays
+var colors = ['red', 'blue', 'green', 'yellow'];
+var names = ['Ed', 'Jae', 'Ricky', 'Four'];
+
+// Add code which adds a color and a name to the respective arrays
+colors.push('Violet');
+names.push('Seabiscuit');
+
+console.log(colors);
+console.log(names);
