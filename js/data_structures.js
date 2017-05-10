7c02f6f64d73315e553f31638eeb2fe2771ee8f6
rickyveltri <rickveltri@gmail.com>
2017-05-10 12:51:04 -0700

    Create a car constructor, a few cars, and test their functionality

--- js/data_structures.js
+++ js/data_structures.js
@@ -15,3 +15,30 @@ for (var i = 0; i < colors.length; i++) {
 console.log(Horses);
 
 
+// create a car using a constuctor function with a few different properties
+// include one function
+
+function Car(color, year, automatic) {
+    this.color = color;
+    this.year = year;
+    this.automatic = automatic;
+
+    this.startEngine = function startEngine() {
+        console.log("Vroom vroom")
+    }
+}
+
+
+// create a few cars and test their startEngine function
+var mustang = new Car('red', 2015, false);
+var corolla = new Car('Black', 2010, true);
+var yukon = new Car('Silver', 2000, true);
+
+console.log(mustang);
+mustang.startEngine();
+
+console.log(mustang);
+corolla.startEngine();
+
+console.log(mustang);
+yukon.startEngine();
