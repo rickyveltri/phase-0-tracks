baf748a38cb1db8d0a7d641f8f534b86b3a709fd
rickyveltri <rickveltri@gmail.com>
2017-05-09 19:29:15 -0700

    Add reflections

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -97,3 +97,27 @@ end
 
 #=======================================================================
 # Reflection Section
+=begin
+
+
+Hash:  string vs. symbol
+-- String keys create a new instance variable, even if it is the exact same.  In the case of the state name, using strings seems to be ideal; some names contain spaces, as well as for aesthetic purposes (upcasing proper names).  A new object is created for each one.
+-- For the keys within the hash (i.e. population_density + population), the symbol format was used.  This format is ideal, as the ruby language recognizes that they represent the same data, relate to the same object, and in turn, do not use unneccessary memory creating superfluous objects.
+
+require_relative vs require
+-- 'require_relative' creates links between files of the same folder.  We used 'require_relative' above to link the state_data.rb file and information to our code in my_solution.rb.
+-- 'require' gives the ability to create links to gems and extra libraries of code not readily available by Ruby's default configuration.
+
+Hash iteration
+-- the built-in method of '.each' is a great way to iterate through a hash
+-- we used the .each method above to create the class instances (states) and run the virus_effects method for each.
+
+virus_effects - what stood out
+-- The variables were unneccessary arguments in both the virus_effects method and sub-methods
+
+Most solidified concept:
+-- Refactoring
+-- In particular, there was a great difference in the readability of the 'predicted_deaths' method before and after refactoring
+
+
+=end
