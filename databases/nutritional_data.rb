268c16775aa0262076c3533b03b89fe55563a67b
rickyveltri <rickveltri@gmail.com>
2017-05-22 22:49:24 -0700

    Add table creation commands

--- databases/nutritional_data.rb
+++ databases/nutritional_data.rb
@@ -4,3 +4,45 @@ require 'sqlite3'
 
 #Create SQLite database
 db = SQLite3::Database.new( "nutritional_data.db" )
+
+#Create produce table (e.g. greens; potatoes)
+create_produce_table_cmd = <<-SQL
+    CREATE TABLE IF NOT EXISTS produce (
+        id INTEGER PRIMARY KEY,
+        name VARCHAR(255),
+        calories INT,
+        protein INT,
+        carbs INT,
+        fat INT
+    )
+SQL
+
+#Create animals table (e.g. meat; fish)
+create_animals_table_cmd = <<-SQL
+    CREATE TABLE IF NOT EXISTS animals (
+        id INTEGER PRIMARY KEY,
+        name VARCHAR(255),
+        calories INT,
+        protein INT,
+        carbs INT,
+        fat INT
+    )
+SQL
+
+#create fats table (e.g. nuts; oils)
+create_fats_table_cmd = <<-SQL
+    CREATE TABLE IF NOT EXISTS fats (
+        id INTEGER PRIMARY KEY,
+        name VARCHAR(255),
+        calories INT,
+        protein INT,
+        carbs INT,
+        fat INT
+    )
+SQL
+
+
+#Driver code
+db.execute(create_produce_table_cmd)
+db.execute(create_animals_table_cmd)
+db.execute(create_fats_table_cmd)
